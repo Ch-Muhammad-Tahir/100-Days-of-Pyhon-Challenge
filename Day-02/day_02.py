f04f2fe38e0ad9353f71a88a18c9350a9947e69f
Ch-Muhammad-Tahir <tahir.ctn001@gmail.com>
2022-05-31 23:26:32 +0500

    Day 2 BMI Calcualtor Complete

---
+++ Day-02/day_02.py
@@ -0,0 +1,37 @@
+# Type checking Fucntion
+print(type('Hello'))
+num=123
+print(type(num))
+
+name= input("Enter Your Name: ")
+print(type(name))
+
+# Mathematical Operations
+# 6+9
+# 5-2
+# 9*3
+# 9/3
+# 2**2
+
+# Prioritys
+# PEMDS
+# ()
+# **
+# * /
+# + -
+
+print(2**3)  # Answer 8
+
+print(8//3)
+
+score = 0
+print(score)
+
+score += 1
+print(score)
+
+# f-String
+height = 5.6
+is_winnig =True
+
+print(f"your Socer is {score}, your Height is {height}, you are winning is {is_winnig}.")
