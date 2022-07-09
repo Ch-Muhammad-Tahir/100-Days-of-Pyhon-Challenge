50d0c960ea4f324a4340d356a457f5b7036ba5b9
Ch-Muhammad-Tahir <tahir.ctn001@gmail.com>
2022-07-09 23:57:03 +0500

    Day-04 Project

--- Day-04/Day-04-Rock-Paper-Scissors.py
+++ Day-04/Day-04-Rock-Paper-Scissors.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -25,9 +27,17 @@ scissors = '''
 ---.__(___)
 '''
 
-#Write your code below this line 👇
+# Write your code below this line 👇
 
 player_choose = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.")
-game_items =[rock,paper,scissors]
+game_items = [rock, paper, scissors]
+if player_choose == "0":
+    print(game_items[0])
+elif player_choose == "1":
+    print(game_items[1])
+elif player_choose == "2":
+    print(game_items[2])
 
-
+print("Computer Choose ")
+computer_choose = random.choice(game_items)
+print(computer_choose)
